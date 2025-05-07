3892573388c92c0d0681ac4706d0a778febc5794
Raja Pandi <114086052+Rawwire@users.noreply.github.com>
2025-05-07 21:01:31 +0530

    Update balan.py

--- balan.py
+++ balan.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="Balan Website", page_icon=":heart:", layout="wide")
 
-st.title("Balan Website")
+st.title("Balan Website, Hello")
 st.write("Welcome to the Balan website! This is a simple web application built using Streamlit.")
